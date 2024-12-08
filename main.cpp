117169d31c36c2e4d7e6549516f16cb0d7b77682
Lincoln Deen <ldeen2@zagmail.gonzaga.edu>
2024-12-08 15:12:55 -0800

    added good data

--- main.cpp
+++ main.cpp
@@ -12,62 +12,44 @@ int main() {
     // Create a BinaryTree object
     BinaryTree tree;
 
+    tree.add("Columbia River", "root", "Length: 1243 miles, Dam: Bonneville Dam"); 
 
+    // Add major tributaries using addAt and searchNode
+    Node* columbia = tree.searchNode(tree.getRoot(), "Columbia River"); 
 
-    // Add the Columbia River as the root (using add)
-    tree.add("Columbia River", "root", "Length: 1243 miles, Dam: Bonneville Dam (Washington/Oregon border)");
+    // Level 2
+    tree.addAt(columbia, tree.getParent(columbia), "Willamette River", "left", "Length: 301 miles, Dam: Detroit Dam, Confluence: Willamette Falls"); 
+    tree.addAt(columbia, tree.getParent(columbia), "Snake River", "right", "Length: 1078 miles, Dam: Hells Canyon Dam"); 
 
-    // Add major tributaries using addAt and getParent
-    Node* columbia = tree.getRoot(); 
+    // Level 3
+    Node* willamette = tree.searchNode(tree.getRoot(), "Willamette River"); 
+    Node* snake = tree.searchNode(tree.getRoot(), "Snake River");
+    tree.addAt(willamette, tree.getParent(willamette), "McKenzie River", "left", "Length: 90 miles, Dam: Cougar Dam");
+    tree.addAt(willamette, tree.getParent(willamette), "Clackamas River", "right", "Length: 83 miles, Dam: North Fork Dam"); 
+    tree.addAt(snake, tree.getParent(snake), "Clearwater River", "left", "Length: 75 miles, Dam: Dworshak Dam");
+    tree.addAt(snake, tree.getParent(snake), "Salmon River", "right", "Length: 425 miles"); 
 
-    // Left tributaries
-    tree.addAt(columbia, tree.getParent(columbia), "Snake River", "left", "Length: 1078 miles, Dam: Hells Canyon Dam (Idaho/Oregon border)");
-    tree.addAt(columbia, tree.getParent(columbia), "John Day River", "right", "Length: 281 miles, Dam: John Day Dam (Oregon/Washington border)");
-
-    // Snake River tributaries
-    Node* snake = tree.searchNode(tree.getRoot(), "Snake River"); 
-    tree.addAt(snake, tree.getParent(snake), "Clearwater River", "right", "Length: 74 miles");
-    tree.addAt(snake, tree.getParent(snake), "Salmon River", "left", "Length: 425 miles");
-
-    // Clearwater River tributaries
+    // Level 4
+    Node* mckenzie = tree.searchNode(tree.getRoot(), "McKenzie River");
+    Node* clackamas = tree.searchNode(tree.getRoot(), "Clackamas River");
     Node* clearwater = tree.searchNode(tree.getRoot(), "Clearwater River");
-    tree.addAt(clearwater, tree.getParent(clearwater), "North Fork Clearwater River", "left", "Length: 75 miles");
-    tree.addAt(clearwater, tree.getParent(clearwater), "Little North Fork Clearwater River", "right", "Length: 35 miles");
-
-    // North Fork Clearwater River tributaries
-    Node* northForkClearwater = tree.searchNode(tree.getRoot(), "North Fork Clearwater River");
-    tree.addAt(northForkClearwater, tree.getParent(northForkClearwater), "Kelly Creek", "right", "Length: 25 miles");
-    tree.addAt(northForkClearwater, tree.getParent(northForkClearwater), "Orofino Creek", "left", "Length: 21 miles");
-
-    // Salmon River tributaries
     Node* salmon = tree.searchNode(tree.getRoot(), "Salmon River");
-    tree.addAt(salmon, tree.getParent(salmon), "Middle Fork Salmon River", "right", "Length: 113 miles");
-    tree.addAt(salmon, tree.getParent(salmon), "South Fork Salmon River", "left", "Length: 61 miles");
+    tree.addAt(mckenzie, tree.getParent(mckenzie), "Blue River", "left", "Length: 27 miles");  
+    tree.addAt(clackamas, tree.getParent(clackamas), "Oak Grove Fork Clackamas River", "left", "Length: Unknown"); 
+    tree.addAt(clearwater, tree.getParent(clearwater), "North Fork Clearwater River", "left", "Length: 75 miles"); 
+    tree.addAt(clearwater, tree.getParent(clearwater), "Middle Fork Clearwater River", "right", "Length: 86 miles, Confluence: Kooskia, Idaho"); 
+    tree.addAt(salmon, tree.getParent(salmon), "Middle Fork Salmon River", "left", "Length: 104 miles"); 
+    tree.addAt(salmon, tree.getParent(salmon), "South Fork Salmon River", "right", "Length: 86 miles"); 
 
-    // Middle Fork Salmon River tributaries
-    Node* middleForkSalmon = tree.searchNode(tree.getRoot(), "Middle Fork Salmon River");
-    tree.addAt(middleForkSalmon, tree.getParent(middleForkSalmon), "Valley Creek", "left", "Length: 17 miles");
-    tree.addAt(middleForkSalmon, tree.getParent(middleForkSalmon), "Bear Valley Creek", "right", "Length: 31 miles");
-
-    // John Day River tributaries
-    Node* johnDay = tree.searchNode(tree.getRoot(), "John Day River");
-    tree.addAt(johnDay, tree.getParent(johnDay), "North Fork John Day River", "left", "Length: 122 miles");
-    tree.addAt(johnDay, tree.getParent(johnDay), "Middle Fork John Day River", "right", "Length: 63 miles");
-
-    // North Fork John Day River tributaries
-    Node* northForkJohnDay = tree.searchNode(tree.getRoot(), "North Fork John Day River");
-    tree.addAt(northForkJohnDay, tree.getParent(northForkJohnDay), "Desolation Creek", "right", "Length: 15 miles");
-    tree.addAt(northForkJohnDay, tree.getParent(northForkJohnDay), "Camas Creek", "left", "Length: 14 miles");
-
-    // Desolation Creek tributaries
-    Node* desolationCreek = tree.searchNode(tree.getRoot(), "Desolation Creek");
-    tree.addAt(desolationCreek, tree.getParent(desolationCreek), "Big Creek", "left", "Length: 11 miles");
-    tree.addAt(desolationCreek, tree.getParent(desolationCreek), "Granite Creek", "right", "Length: 11 miles");
-
-    // Middle Fork John Day River tributaries
-    Node* middleForkJohnDay = tree.searchNode(tree.getRoot(), "Middle Fork John Day River");
-    tree.addAt(middleForkJohnDay, tree.getParent(middleForkJohnDay), "Lake Creek", "left", "Length: 14 miles");
-    tree.addAt(middleForkJohnDay, tree.getParent(middleForkJohnDay), "South Fork John Day River", "right", "Length: 74 miles");
+    // Level 5 
+    Node* blue = tree.searchNode(tree.getRoot(), "Blue River");
+    Node* northForkClearwater = tree.searchNode(tree.getRoot(), "North Fork Clearwater River");
+    Node* middleForkClearwater = tree.searchNode(tree.getRoot(), "Middle Fork Clearwater River");
+    Node* middleForkSalmon = tree.searchNode(tree.getRoot(), "Middle Fork Salmon River"); 
+    tree.addAt(blue, tree.getParent(blue), "South Fork McKenzie River", "left", "Length: Unknown"); 
+    tree.addAt(northForkClearwater, tree.getParent(northForkClearwater), "Lochsa River", "left", "Length: 70 miles"); 
+    tree.addAt(middleForkClearwater, tree.getParent(middleForkClearwater), "Lochsa River", "right", "Length: 70 miles, Confluence: Lowell, Idaho"); 
+    tree.addAt(middleForkSalmon, tree.getParent(middleForkSalmon), "Little Salmon River", "left", "Length: 161 miles"); 
 
 
 
